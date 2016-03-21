4b1564c356a0ec158a88b733c5622463c9d588ed
Owen Klaiss <oklaiss@smu.edu>
2016-03-21 14:50:46 -0500

    add cron functionality, needs API calls next

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,16 @@
 class Notification < ActiveRecord::Base
 	belongs_to :user
+
+	def self.send_notifications
+		self.find_each do |notification|
+			# do stuff here
+			# check if needs to be sent
+			notification.send
+		end
+	end
+
+	def send
+		#make api calls
+	end
+
 end
