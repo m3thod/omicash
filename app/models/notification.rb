6056dba8b89e9f320fbb0e7fe90b743d5abdf752
Owen Klaiss <oklaiss@smu.edu>
2016-03-21 22:03:58 -0500

    added initial twilio API integration

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -11,6 +11,19 @@ class Notification < ActiveRecord::Base
 
 	def send
 		#make api calls
+		account_sid = 'ACb7ab149eb0706c444c6fe00f688c45de'
+		auth_token = '99090f489e89597e5f58cda4adee0bef'
+		to_number = self.phone_number
+		message_body = self.message_text
+
+		# set up a client to talk to the Twilio REST API
+		@client = Twilio::REST::Client.new account_sid, auth_token
+
+		@client.account.messages.create({
+			:from => '+16503810319',
+			:to => to_number,
+			:body => message_body,
+		})
 	end
 
 end
