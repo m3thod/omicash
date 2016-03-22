7f5b26d2d8f6cdb07a4504a23cb6dd045f09b54f
Owen Klaiss <oklaiss@smu.edu>
2016-03-21 21:56:59 -0500

    add images and styling minus fonts

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
+  # static pages
+  get "/pages/:page" => "pages#show"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
