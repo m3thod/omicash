bc0fd99e0a87b5fd454de97f710e054ad72a93cb
Owen Klaiss <oklaiss@smu.edu>
2016-04-16 14:05:15 -0500

    updates to styling for form handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,15 @@ class UsersController < ApplicationController
 	end
 
 	def update
+		respond_to do |format|
+      if @user.update(notification_params)
+        format.html { redirect_to notifications_path, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
 	end
 
 	private
