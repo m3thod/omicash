54d416a2742670ed1ca8ea334fe6a0e825ad0c44
Owen Klaiss <oklaiss@smu.edu>
2016-04-20 21:02:30 -0500

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'notifications#index'
+  root 'notifications#index'
   # root 'pages#home'
-  root 'sessions#new'
+  # root 'sessions#new'
 
   # authenticated :user do
   #   root :to => "notifications#index"
